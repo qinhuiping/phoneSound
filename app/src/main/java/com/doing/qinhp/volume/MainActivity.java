457fccf36231d8bea58d1850aa8ef80e297e77a5
qinhp <qhp603560399@163.com>
2019-03-29 14:23:58 +0800

    Initial commit

---
+++ app/src/main/java/com/doing/qinhp/volume/MainActivity.java
@@ -0,0 +1,63 @@
+package com.doing.qinhp.volume;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+
+    private TextView pb_tv;
+    private TextView v_tv;
+    private TextView min;
+    private TextView max;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seekBar = findViewById(R.id.sb_progress);
+        pb_tv = findViewById(R.id.sb_progress_view);
+        v_tv = findViewById(R.id.tv_volume);
+        min = findViewById(R.id.tv_minvolume);
+        max = findViewById(R.id.tv_maxvolume);
+
+
+        final AudioManager mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int currentVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC );
+        seekBar.setMax(mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+        seekBar.setProgress(currentVolume);
+        pb_tv.setText("当前音量" + currentVolume);
+        v_tv.setText("当前progress" + currentVolume);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            min.setText("最小音量" + mAudioManager.getStreamMinVolume(AudioManager.FLAG_PLAY_SOUND));
+        }
+        max.setText("最大音量" + mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC , progress, 50);
+                pb_tv.setText("当前音量" + mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC ));
+                v_tv.setText("当前progress" + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
